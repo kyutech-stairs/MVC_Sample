a0741c1347a175715fabf8a2486bf5b37fd82239
Azuma Sato <lifeghtcloerce5291@gmail.com>
2018-09-27 14:13:54 +0900

    add Files

---
+++ MVC_Sample/MVC_Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC_Sample
+//
+//  Created by Azuma on 2018/09/27.
+//  Copyright © 2018 Azuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
