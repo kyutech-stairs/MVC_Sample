b3b24d24f84499d5170ba545fdc2ed94652f85f4
azuma sato <lifeghtcloerce5291@gmail.com>
2018-10-09 16:07:43 +0900

    Update Result.swift

--- MVC_Sample/MVC_Sample/Model/Result.swift
+++ MVC_Sample/MVC_Sample/Model/Result.swift
@@ -9,37 +9,10 @@
 import Foundation
 
 struct Result: Decodable {
-    var totalCount: Int
-    var items: [Item] = []
-    
-    private enum CodingKeys: String, CodingKey {
-        case items
-        case totalCount = "total_count"
-    }
 }
 
 struct Item: Decodable {
-    var name: String
-    var owner: Owner
-    var html: String
-    var starCount: Int
-    
-    private enum CodingKeys: String, CodingKey {
-        case name
-        case owner
-        case html = "html_url"
-        case starCount = "stargazers_count"
-    }
 }
 
 struct Owner: Decodable {
-    var id: Int
-    var login: String
-    var avatarURL: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case login
-        case avatarURL = "avatar_url"
-    }
 }
