b7c62e0dda5287db885d8c83239d040b98ea751c
azuma sato <lifeghtcloerce5291@gmail.com>
2018-10-09 16:07:16 +0900

    Update Repository.swift

--- MVC_Sample/MVC_Sample/Model/Repository.swift
+++ MVC_Sample/MVC_Sample/Model/Repository.swift
@@ -7,42 +7,6 @@
 //
 
 import Foundation
-import UIKit
 
 class Repository {
-    var name: String
-    var owner: String
-    var starCount: Int
-    var imagePath: String
-    var path: String
-    
-    init(name: String, owner: String, starCount: Int, imagePath: String, path: String) {
-        self.name = name
-        self.owner = owner
-        self.starCount = starCount
-        self.imagePath = imagePath
-        self.path = path
-    }
-    
-    class func fetchRepository(q: String, completion: @escaping ([Repository]) -> Void) {
-        let url: String = "https://api.github.com/search/repositories?q=\(q)&sort=stars"
-        URLSession.shared.dataTask(with: URL(string: url)!) { (data, response, error) in
-            if error == nil, let data = data {
-                let decoder = JSONDecoder()
-                do {
-                    let result: Result = try decoder.decode(Result.self, from: data)
-                    var repositories: [Repository] = []
-                    _ = result.items.map ({
-                        let repository = Repository(name: $0.name, owner: $0.owner.login, starCount: $0.starCount, imagePath: $0.owner.avatarURL, path: $0.html)
-                        repositories.append(repository)
-                    })
-                    completion(repositories)
-                } catch(let err) {
-                    print(err)
-                }
-            } else {
-                print(error!)
-            }
-            }.resume()
-    }
 }
